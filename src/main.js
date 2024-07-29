c828b09b21c9a9dc49367dbcd8ba0a85ad20ff8b
annaannaA33 <annaansmva@gmail.com>
2024-07-29 12:07:53 +0300

    new framework instaled

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/themes.css'
 import './assets/global.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives
+})
+createApp(App).use(vuetify).mount('#app')
 app.use(router)
 
 app.mount('#app')
